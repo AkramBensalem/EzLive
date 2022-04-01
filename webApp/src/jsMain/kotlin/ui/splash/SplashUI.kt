b539be75b89956bcbb9dd5aa633c6d1ab734f255
Bensalem Akram <ak.bensalem@esi-sba.dz>
2022-04-01 04:42:45 -0700

    JS components

--- webApp/src/jsMain/kotlin/ui/splash/SplashUI.kt
+++ webApp/src/jsMain/kotlin/ui/splash/SplashUI.kt
@@ -1,34 +1,50 @@
 package ui.splash
 
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
 import dz.esi.sba.ezlive.shared.presenters.splash.SplashScreenInterface
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import org.jetbrains.compose.web.css.*
+import org.jetbrains.compose.web.css.keywords.auto
 import org.jetbrains.compose.web.dom.Button
 import org.jetbrains.compose.web.dom.Div
 import org.jetbrains.compose.web.dom.Text
+import ui.components.layouts.ContainerInSection
+import ui.theme.colomun.WtCols
+import ui.theme.row.WtRows
+import utils.extensions.subscribeAsState
 
 @Composable
 fun SplashUI(
     component: SplashScreenInterface
-){
-    Div(
-        attrs = {
-            onClick {
-                component.onTimeFinished(1)
+) {
+    //
+
+    val model: SplashScreenInterface.Model by component.model.subscribeAsState()
+
+    val scope = rememberCoroutineScope()
+
+
+    LaunchedEffect(Unit) {
+        scope.launch {
+            while (true) {
+                delay(500)
+                component.onUpdateTitle()
             }
         }
-    ) {
-        Button(attrs = {
-            style {
-                onClick {
-                    component.onTimeFinished(1)
+    }
+
+    ContainerInSection {
+        Div({ }) {
+                Button(attrs = {
+                    style {
+                        onClick {
+                            component.onTimeFinished(1)
+                        }
+                    }
+                }) {
+                    Text("Click me  ${model.title}")
                 }
-            }
-        }) {
-            Text("Click me")
         }
     }
 }
